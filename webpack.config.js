1d85ade3ca78c9997cc0e9f8176f17926552e2ff
ctw <homedown_90@163.com>
2017-12-25 06:20:12 +0000

    添加bootstrap 环境

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+module.exports = {
+    entry: __dirname + "/main.js",
+    output: {
+        path: __dirname ,
+        filename: "bundle.js"
+    },
+    module:
+        {
+            rules:
+                [
+                    {
+                        test: /(\.scss|\.css)$/,
+                        use: [{
+                            loader: 'style-loader', // inject CSS to page
+                        }, {
+                            loader: 'css-loader', // translates CSS into CommonJS modules
+                        }, {
+                            loader: 'postcss-loader', // Run post css actions
+                            options: {
+                                plugins: function () { // post css plugins, can be exported to postcss.config.js
+                                    return [
+                                        require('precss'),
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        }, {
+                            loader: 'sass-loader' // compiles SASS to CSS
+                        }]
+                    },
+                ]
+        },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+            Util: "exports-loader?Util!bootstrap/js/dist/util",
+            Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+        }),
+    ]
+}
