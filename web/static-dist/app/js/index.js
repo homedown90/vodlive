03e1fc249ae8a39f8966aa4da6e7eeea28f0cd86
changtao <changtao@xiaomi.com>
2017-12-28 18:58:46 +0800

    添加前端首页

--- web/static-dist/app/js/index.js
+++ web/static-dist/app/js/index.js
@@ -3,3 +3,9 @@ import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/dropdown';
 
 import '../css/common.css';
+$(function () {
+    $('#myTab a').on('mousemove', function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+});
